4216315fc9bd6027f9aa97f587c4eaa1f9facbb5
Gaikwad, Sachin (Pune) <Sachin.Gaikwad@fiserv.com>
2022-01-27 08:00:35 -0500

    Catlog microservices

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            MongoClient dbClient = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var dataBase = dbClient.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = dataBase.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
